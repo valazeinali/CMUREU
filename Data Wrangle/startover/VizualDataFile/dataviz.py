2d2446944f52a85c076f5232213aa0a65714d059
Vala Zeinali <32586254+valazeinali@users.noreply.github.com>
2019-06-27 09:55:42 -0400

    Add files via upload
    Data gets best results

--- Data Wrangle/startover/VizualDataFile/dataviz.py
+++ Data Wrangle/startover/VizualDataFile/dataviz.py
@@ -1,3 +1,4 @@
+'''
 import matplotlib.pyplot as plt
 import csv
 
@@ -17,3 +18,39 @@ plt.title(row[1])
 plt.legend()
 plt.show()
 #3999999999
+'''
+
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#s = line[1].value_counts() ## Counts the occurrence of unqiue elements and stores in a variable called "s" which is series type
+#$new = pd.DataFrame({'FuncGroup':s.index, 'Count':s.values})  ## Converting series type to pandas df as plotly accepts dataframe as input. The two columns of df is FuncGroup which is being made by index of series and new variable called count which is made by values of series s.
+#path = 'c:\\temp\\'
+x = []
+y = []
+
+file=open( "vizPR1.CSV", "r")
+reader = csv.reader(file)
+data_list = list(csv.reader('vizPR1.CSV'))
+for i in data_list:
+	print(i)
+
+    #s = i[1].value_counts()
+new = pd.DataFrame({'FuncGroup':s.index, 'Count':s.values}) 
+
+for line in reader:
+
+    t=line[0],line[1]
+
+    x.append(line[0])
+    y.append(line[1])
+
+    print(t)
+
+plt.plot(x,y, label='Loaded from file!')
+plt.xlabel('month')
+plt.ylabel('frequency')
+plt.title("A-NLL")
+plt.legend()
+plt.show()
